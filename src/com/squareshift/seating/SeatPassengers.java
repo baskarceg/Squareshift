10825def224e63cd25d2cbc69a724d4d7546caa8
baskarceg <61894344+baskarceg@users.noreply.github.com>
2021-12-02 08:06:25 +0530

    Adding output image for ReadMe

--- src/com/squareshift/seating/SeatPassengers.java
+++ src/com/squareshift/seating/SeatPassengers.java
@@ -4,7 +4,7 @@ public class SeatPassengers {
 
 	public static void main(String[] args) {
 		
-		int[][] seatArray = {{3,2},{4,3},{2,3},{3,4}};
+		int[][] seatArray = {{3,2},{4,3},{2,3},{3,4},{2,2}};
 		int noOfPassengers = 30;
 		
 		AirplaneSeating.seatPassengers(seatArray, noOfPassengers);
